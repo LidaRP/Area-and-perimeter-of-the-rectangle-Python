11769b8e7c1f0615822fe98a0c13305b251faba3
LidaRP <rahmanipiyani.lida@gmail.com>
2022-05-10 13:02:13 +0430

    Area and perimeter of the rectangle

---
+++ Area and perimeter of the rectangle.py
@@ -0,0 +1,8 @@
+a = float (input('Please enter the length of the rectangle : '))
+b = float (input('Please enter the width of the rectangle: '))
+
+perim = (a + b) * 2
+area = a * b
+
+print('The perimeter of a rectangle is equal to : ',perim)
+print('The area of a rectangle is equal to : ',area)
